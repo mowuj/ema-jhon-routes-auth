206a77f5f9884df4d7c10009c1b8e89b5aef3352
mowuj <mowuj92@gmail.com>
2022-04-02 00:01:00 +0600

    review cart

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>We are secret</h1>
+            <p>but you can know us</p>
+        </div>
+    );
+};
+
+export default About;
