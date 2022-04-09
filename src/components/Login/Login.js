47ae6fabc09ba02e7bf2b48902ef6ae8aa3ffa45
mowuj <mowuj92@gmail.com>
2022-04-10 00:20:43 +0600

    style sign up form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,22 +10,24 @@ const Login = () => {
                 <form action="">
                     <div className="input-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="" />
+                <input type="email" name="email" id="" required/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="" />
+                    <input type="password" name="password" id="" required/>
                 </div>
                 <input className='form-submit' type="submit" value="Login" />
                 </form>
-                <p className='signup-p'>
+                <p className='account-text'>
                     New to Ema-John?<Link className='form-link' to="/signup">Create an account</Link>
                 </p>
-                <p className='signup-p'>---------------or---------------</p>
-                <div className='icon-container'>
-                    <p ><FcGoogle className='icon'></FcGoogle></p>
-                    <p className='icon-text'> Continue with Google</p>
+                <div className='or'>
+                    <div className='or-style'></div>
+                    or
+                    <div className='or-style'></div>
                 </div>
+                    <button className='google-btn'><FcGoogle className='icon'></FcGoogle> <span className='icon-text'>Continue with Google</span> </button>
+                
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
 const SignUp = () => {
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form action="">
+                    <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="" required/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input type="password" name="confirm-password" id="" required/>
+                </div>
+                <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p className='account-text'>
+                    Already Have an account?<Link className='form-link' to="/login">Login</Link>
+                </p>
+                <div className='or'>
+                    <div className='or-style'></div>
+                    or
+                    <div className='or-style'></div>
+                </div>
+                    <button className='google-btn'><FcGoogle className='icon'></FcGoogle> <span className='icon-text'>Continue with Google</span> </button>
+                
+            </div>
         </div>
     );
 };
